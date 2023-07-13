8230d3aa0796aa6dae340a72c81f3a2c9b6d0b82
55154M <houssem.sakher@maif.fr>
2023-07-13 20:12:06 +0200

    Adding jms config sender and listener and task config as well

---
+++ src/main/java/guru/framework/model/HelloWorldMessage.java
@@ -0,0 +1,17 @@
+package guru.framework.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+import java.util.UUID;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class HelloWorldMessage {
+    private UUID id;
+    private String message;
+}
